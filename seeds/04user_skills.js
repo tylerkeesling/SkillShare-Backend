6f381196745789c007fb9a16b399f9f6454b3cfa
tkeesling <tyler.keesling@gmail.com>
2017-08-05 15:39:12 -0600

    idk

--- seeds/04user_skills.js
+++ seeds/04user_skills.js
@@ -1,17 +1,43 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex.raw('DELETE FROM user_skills; ALTER SEQUENCE user_skills_id_seq RESTART WITH 5')
-    .then(function () {
-      // Inserts seed entries
-      return knex('user_skills').insert([
-        {id: 1, users_id: 1 , skills_id:4},
-        {id: 2, users_id: 1 , skills_id:2},
-        {id: 3, users_id: 2 , skills_id:1},
-        {id: 4, users_id: 3 , skills_id:1},
-        {id: 5, users_id: 3 , skills_id:5},
-        {id: 6, users_id: 4 , skills_id:3},
-        {id: 7, users_id: 4 , skills_id:2}
-      ]);
-    });
+	// Deletes ALL existing entries
+	return knex.raw('DELETE FROM user_skills; ALTER SEQUENCE user_skills_id_seq RESTART WITH 5')
+		.then(function() {
+			// Inserts seed entries
+			return knex('user_skills').insert([{
+					id: 1,
+					users_id: 1,
+					skills_id: 4
+				},
+				{
+					id: 2,
+					users_id: 1,
+					skills_id: 2
+				},
+				{
+					id: 3,
+					users_id: 2,
+					skills_id: 1
+				},
+				{
+					id: 4,
+					users_id: 3,
+					skills_id: 1
+				},
+				{
+					id: 5,
+					users_id: 3,
+					skills_id: 5
+				},
+				{
+					id: 6,
+					users_id: 4,
+					skills_id: 3
+				},
+				{
+					id: 7,
+					users_id: 4,
+					skills_id: 2
+				}
+			]);
+		});
 };
