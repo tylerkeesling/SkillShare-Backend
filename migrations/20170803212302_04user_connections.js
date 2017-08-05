14aacdd6a65d333c5146fddc1ed56c55fc52c969
tkeesling <tyler.keesling@gmail.com>
2017-08-05 13:19:24 -0600

    after merge, deleted table changes

--- migrations/20170803212302_04user_connections.js
+++ migrations/20170803212302_04user_connections.js
@@ -3,9 +3,6 @@ exports.up = function(knex, Promise) {
 		table.increments()
 		table.integer('user1_id').references('users.id')
 		table.integer('user2_id').references('users.id')
-		table.integer('user1_learn').references('users.skill_learn')
-		table.integer('user2_learn').references('users.skill_learn')
-		table.boolean('match').false
 	})
 };
 
