49af8cfd97b614ccae5ed5ed21629f96faf51cda
tkeesling <tyler.keesling@gmail.com>
2017-08-05 13:20:17 -0600

    before merge

--- migrations/20170803212302_04user_connections.js
+++ migrations/20170803212302_04user_connections.js
@@ -1,12 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('user_connections' , (table) => {
-    table.increments()
-    table.integer('user1_id').references('users.id')
-    table.integer('user2_id').references('users.id')
-  })
+	return knex.schema.createTable('user_connections', (table) => {
+		table.increments()
+		table.integer('user1_id').references('users.id')
+		table.integer('user2_id').references('users.id')
+	})
 };
 
 exports.down = function(knex, Promise) {
-return knex.schema.dropTableIfExists('user_connections')
+	return knex.schema.dropTableIfExists('user_connections')
 };
