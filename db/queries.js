e2df13d1ad4fbbc1858084022987e0478d0fde55
yousif al timimie <yaltimimie@gmail.com>
2017-08-05 13:39:12 -0600

    getNamesOfUserSkills

--- db/queries.js
+++ db/queries.js
@@ -14,6 +14,9 @@ module.exports = {
 		return knex('user_skills')
 	},
 	getUserSkillsById: function(id) {
-		return knex('user_skills').where('users_id', id)
+		return knex('user_skills')
+    .join('skills','skills.id','user_skills.skills_id')
+    .where('user_skills.users_id', id)
+    .select('skills.id','skills.name')
 	}
 }
