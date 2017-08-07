db4bebb4ba273c36a1685facf46ba39be9b2f78d
tkeesling <tyler.keesling@gmail.com>
2017-08-07 10:04:35 -0600

    before merge

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,6 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
-router.get('/', function(req, res) {
-	res.json({
-		message: 'working'
-	})
-})
-
 router.post('/login', function(req, res, next) {
 	knex('users').where('username', req.body.username)
 		.then(user => {
@@ -36,26 +30,3 @@ router.post('/login', function(req, res, next) {
 })
 
 module.exports = router
-
-// 	.then(user => {
-// 		// do they exist
-// 		if (user.length === 0) {
-// 			res.json({
-// 				error: 'Email or password did not match.'
-// 			})
-// 		} else {
-// 			var match = bcrypt.compareSync(req.body.password, user[0].password)
-// 			if (match) {
-// 				// login
-// 				delete user[0].password
-// 				var token = jwt.sign(user[0], 'secret_token')
-// 				res.json({
-// 					data: token
-// 				})
-// 			} else {
-// 				res.json({
-// 					error: 'Email or password did not match.'
-// 				})
-// 			}
-// 		}
-// 	})
