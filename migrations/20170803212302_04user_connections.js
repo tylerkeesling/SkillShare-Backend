dbc26663b5431ce3e327b9c84984b6d72551ddf2
yousif al timimie <yaltimimie@gmail.com>
2017-08-08 22:15:23 -0600

    validation on connections

--- migrations/20170803212302_04user_connections.js
+++ migrations/20170803212302_04user_connections.js
@@ -4,7 +4,11 @@ exports.up = function(knex, Promise) {
 		table.integer('userSendInvite_id').references('users.id')
 		table.integer('userRecievedInvite_id').references('users.id')
 		table.boolean('acceptStatus')
-	})
+	}).then(function(){
+    return knex.schema.alterTable('user_connections', function(t) {
+  t.unique(['userSendInvite_id', 'userRecievedInvite_id'])
+})
+  })
 };
 
 exports.down = function(knex, Promise) {
